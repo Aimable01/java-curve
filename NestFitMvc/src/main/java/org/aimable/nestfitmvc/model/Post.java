0a24bf866944ca9ecdfd6646fa7ea73cde3cd6b3
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-04 05:55:56 +0200

    feat: post model

---
+++ NestFitMvc/src/main/java/org/aimable/nestfitmvc/model/Post.java
@@ -0,0 +1,68 @@
+package org.aimable.nestfitmvc.model;
+
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+    private String author;
+    private String date;
+
+    public Post(int id, String title, String content, String author, String date) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
