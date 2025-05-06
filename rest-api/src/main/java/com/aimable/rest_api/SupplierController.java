6fab8c24ad81cfb5981eba5a8108448d56062dbf
Aimable01 <aimable.kwizera14@gmail.com>
2025-05-06 08:45:51 +0200

    rest api

---
+++ rest-api/src/main/java/com/aimable/rest_api/SupplierController.java
@@ -0,0 +1,30 @@
+package com.aimable.rest_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/suppliers")
+public class SupplierController {
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @GetMapping()
+    public List<Supplier> findAll() {
+        return supplierService.findAll();
+    }
+
+    @PostMapping()
+    public Supplier save(@RequestBody Supplier supplier) {
+        return this.supplierService.createSupplier(supplier);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Supplier> findById(@PathVariable Long id) {
+        return this.supplierService.findById(id);
+    }
+}
---
+++ rest-api/src/main/java/com/aimable/rest_api/SupplierService.java
@@ -0,0 +1,26 @@
+package com.aimable.rest_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> findAll() {
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> findById(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+    public Supplier createSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+}
