9a22d0bc972974ffd02e269fe126fb680b138a1a
Aimable01 <aimable.kwizera14@gmail.com>
2025-05-27 06:26:28 +0200

    init: server

---
+++ project/NestFit/src/test/java/com/aimable/NestFit/NestFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimable.NestFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
